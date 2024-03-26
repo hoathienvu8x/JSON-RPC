7ace7a7876609c5c5ab0c541d68acf973770e9f6
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2024-03-26 14:10:57 +0700

    Handle with epoll

--- remote.c
+++ remote.c
@@ -6,6 +6,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <fcntl.h>
+#include <sys/epoll.h>
 #include <parson.h>
 
 #define BUFFER_SIZE (4096)
@@ -15,7 +17,7 @@ int main(int argc, char ** argv) {
     printf("Usage: %s <hostname> <port>\n", argv[0]);
     return 1;
   }
-  int fd, port, rc = 0;
+  int fd, pid, port, rc = 0;
   struct sockaddr_in addr;
   struct hostent * srv;
   char buff[BUFFER_SIZE] = {0};
@@ -32,47 +34,80 @@ int main(int argc, char ** argv) {
     rc = -1;
     goto out;
   }
-  memset(&addr, 0, sizeof(addr));
-  memcpy(
-    (void *)srv->h_addr, (void *)&addr.sin_addr.s_addr, srv->h_length
-  );
+  bzero((char *)&addr, sizeof(addr));
+  bcopy ( srv->h_addr, &addr.sin_addr, srv->h_length );
+  addr.sin_family = AF_INET;
   addr.sin_port = htons(port);
   if ((rc = connect(fd, (struct sockaddr *)&addr, sizeof(addr))) < 0) {
     printf("Could not connect to server\n");
     goto out;    
   }
-  req = json_value_init_object();
-  if (!req) {
-    printf("Could not make request\n");
-    goto out;
-  }
-  json_object_set_string(json_object(req), "jsonrpc", "2.0");
-  json_object_set_string(json_object(req), "method", "hello");
-  json_object_set_value(
-    json_object(req), "params", json_value_init_object()
-  );
-  json_object_dotset_string(json_object(req), "params.name", "Join");
-  rc = (int)json_serialize_to_buffer(req, buff, BUFFER_SIZE);
+  rc = fcntl(fd, F_GETFL, 0);
   if (rc < 0) {
-    printf("Failed to make payload\n");
+    printf("Get socket flags failed\n");
     goto out;
   }
-  printf("Sending\n\n%s\n", buff);
-  rc = send(fd, &buff[0], strlen(buff), 0);
-  printf("Sent %d bytes\n", rc);
-  if (rc < 0) {
-    printf("Faild to call procedure\n");
+  if ((rc = fcntl(fd, F_SETFL, rc | O_NONBLOCK)) < 0) {
+    printf("Set nonblocking failed\n");
     goto out;
   }
-  memset(&buff, 0, sizeof(buff));
-  rc = recv(fd, buff, BUFFER_SIZE, 0);
-  if (rc <= 0) {
-    printf("Failed to read result\n");
+  pid = epoll_create(1);
+  if (pid < 0) {
+    rc = -1;
+    printf("Create epoll error\n");
     goto out;
   }
-  buff[rc] = '\0';
-  printf("Result\n\n%s\n", buff);
+  struct epoll_event ev;
+  memset(&ev, 0, sizeof(ev));
+  ev.events = EPOLLIN | EPOLLOUT | EPOLLET;
+  ev.data.fd = fd;
+  if ((rc = epoll_ctl(pid, EPOLL_CTL_ADD, fd, &ev)) < 0) {
+    printf("Add socket to epoll failed\n");
+    goto out;
+  }
+  struct epoll_event evs[1];
+  while (1) {
+    int fds = epoll_wait(pid, evs, 1, -1);
+    int i;
+    for (i = 0; i < fds; i++) {
+      if (evs[i].events & EPOLLOUT) {
+        req = json_value_init_object();
+        if (!req) {
+          printf("Could not make request\n");
+          goto out;
+        }
+        json_object_set_string(json_object(req), "jsonrpc", "2.0");
+        json_object_set_string(json_object(req), "method", "hello");
+        json_object_set_value(
+          json_object(req), "params", json_value_init_object()
+        );
+        json_object_dotset_string(json_object(req), "params.name", "Join");
+        rc = (int)json_serialize_to_buffer(req, buff, BUFFER_SIZE);
+        if (rc < 0) {
+          printf("Failed to make payload\n");
+          goto out;
+        }
+        buff[rc] = '\0';
+        rc = send(evs[i].data.fd, &buff[0], strlen(buff), 0);
+        if (rc < 0) {
+          printf("Faild to call procedure\n");
+          goto out;
+        }
+      } else if (evs[i].events & EPOLLIN) {
+        memset(&buff, 0, sizeof(buff));
+        rc = recv(evs[i].data.fd, buff, BUFFER_SIZE, 0);
+        epoll_ctl(pid, EPOLL_CTL_DEL, evs[i].data.fd, &ev);
+        if (rc <= 0) {
+          printf("Failed to read result\n");
+          goto out;
+        }
+        buff[rc] = '\0';
+        printf("Result\n\n%s\n", buff);
+      }
+    }
+  }
 out:
+  epoll_ctl(pid, EPOLL_CTL_DEL, fd, NULL);
   close(fd);
   if (req) json_value_free(req);
   return rc;
